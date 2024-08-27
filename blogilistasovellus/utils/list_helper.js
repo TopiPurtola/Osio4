38a75e0d0456bba69250033f74102f65d168c087
Topi Purtola <topi.purtola@gmail.com>
2024-08-27 18:19:52 +0300

    4.4 toimii

---
+++ blogilistasovellus/utils/list_helper.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+const dummy = (blogs) => {
+    return 1
+  }
+
+  const totalLikes = (blogs) =>{
+    return blogs.reduce((sum, blog) => sum + blog.likes, 0);
+  }
+  
+  module.exports = {
+    dummy,
+    totalLikes
+  }
