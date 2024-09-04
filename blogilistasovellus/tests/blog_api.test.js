6b136deafbd62ca73359c22d3cf38d27e70dfa0b
Topi Purtola <topi.purtola@gmail.com>
2024-09-04 16:28:36 +0300

    4.9

--- blogilistasovellus/tests/blog_api.test.js
+++ blogilistasovellus/tests/blog_api.test.js
@@ -43,6 +43,16 @@ beforeEach(async () => {
     assert.strictEqual(response.body.length, 2)
   })
 
+  test('id field is id, not _id',),async() => {
+    const response = await api.get('api/blogs')
+    const blogs = response.body
+    
+    blogs.forEach(blog => {
+        assert.ok(blog.id)
+        assert.strictEqual(blog._id,undefined)
+    })
+  }
+
   after(async() =>{
     await mongoose.connection.close()
   })
