0c3e7ac47de46070129d6a7a09ea4bb3ffdcb695
Topi Purtola <topi.purtola@gmail.com>
2024-10-13 13:39:56 +0300

    4.15 toimii

--- blogilistasovellus/app.js
+++ blogilistasovellus/app.js
@@ -2,7 +2,10 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+
 const blogsRouter = require('./controllers/blogi')
+const userRouter = require('./controllers/users')
+
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -25,6 +28,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', userRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- blogilistasovellus/tests/testhelper.js
+++ blogilistasovellus/tests/testhelper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlogs = [
     {
@@ -15,11 +16,25 @@ const initialBlogs = [
     },
 ]
 
+
+const nonExistingId = async () => {
+    const blog = new Blog({ content: 'willremovethissoon' })
+    await blog.save()
+    await blog.deleteOne()
+  
+    return blog._id.toString()
+  }
+
 const blogsInDb = async () => {
     const blogs = await Blog.find({})
     return blogs.map(blog => blog.toJSON())
 }
 
+const usersInDb = async () => {
+    const users = await User.find({})
+    return users.map(user => user.toJSON())
+  }
+
 module.exports = {
-    initialBlogs, blogsInDb
+    initialBlogs, blogsInDb, usersInDb, nonExistingId
 }
