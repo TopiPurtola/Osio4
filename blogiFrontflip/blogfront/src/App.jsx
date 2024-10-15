db3bb1da3ffacfd6ca19a9eea6cdc66ea73635f7
Topi Purtola <topi.purtola@gmail.com>
2024-10-15 14:49:17 +0300

    5.1 toimii

--- blogiFrontflip/blogfront/src/App.jsx
+++ blogiFrontflip/blogfront/src/App.jsx
@@ -1,10 +1,34 @@
 import { useEffect, useState } from "react"
 import yhteys from "./yhteys"
+import loginService from '../services/login.js'
 
 const App = () => {
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
   const [blogit, setBlogit] = useState([]); // Alustetaan tyhjä taulukko
   
 
+
+const handleLogin = async (event) => {
+    event.preventDefault()
+    
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
 
   useEffect(() => {
     yhteys.getAll()
@@ -27,9 +51,29 @@ const App = () => {
       });
   }
 
+  if (user === null){
+    return(
+      <div>
+        <h2>Kirjaudu ineen</h2>
+      <div>
+        Käyttäjänimi 
+        <input type="text" value={username} name="Username" onChange={({target}) => setUsername(target.value)}/>
+      </div>
+      <div>
+        Salasana
+        <input type="password" value={password} name="Password" onChange={({target}) => setPassword(target.value)}/> 
+      </div>
+      <div>
+        <button type="submit" onClick={handleLogin}>Kirjaudu</button>
+      </div>
+      </div>
+    )
+  }
+
   return (
     <div>
       <h1>Blogitekstisovellus!! Wuhuu!</h1>
+      Kirjauduttu sisään käyttäjällä {user.username}
       <h3>Siistit Blogit!!</h3>
       <ul>
         {blogit.map(blogi => (
